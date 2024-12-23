b85c1fe434250ac2253947e9dfa67703b6c9f889
sopa <vnkvd571@yandex.ru>
2024-12-23 15:50:46 +0300

    добавить реализацию валидности данных с помощью аннотаций

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.Duration;
@@ -8,8 +10,15 @@ import java.time.Instant;
 @Data
 public class Film {
     private Long id;
+
+    @NotBlank(message = "Название фильма не может быть пустым")
     private String name;
+
     private String description;
+
+    @NotNull(message = "Дата релиза не может быть пустой")
     private Instant releaseDate;
+
+    @NotNull(message = "Продолжительность фильма должна быть указана")
     private Duration duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,5 +1,8 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.Instant;
@@ -7,8 +10,16 @@ import java.time.Instant;
 @Data
 public class User {
     private Long id;
+
+    @NotBlank(message = "Электронная почта не может быть пустой и должна содержать символ @")
+    @Email(message = "Некорректный формат электронной почты")
     private String email;
+
+    @NotBlank(message = "Логин не может быть пустым и содержать пробелы")
     private String login;
+
     private String name;
+
+    @NotNull(message = "Дата рождения не может быть пустой")
     private Instant birthday;
 }
