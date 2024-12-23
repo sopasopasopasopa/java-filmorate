d9753473009db94d9f446aaeb20b1e54ced85ecb
sopa <vnkvd571@yandex.ru>
2024-12-23 15:32:51 +0300

    создать User и Film (классы - модели данных приложения)

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,14 +2,13 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 
-import java.time.Duration;
 import java.time.Instant;
 
 @Data
-public class Film {
+public class User {
     private Long id;
+    private String email;
+    private String login;
     private String name;
-    private String description;
-    private Instant releaseDate;
-    private Duration duration;
+    private Instant birthday;
 }
