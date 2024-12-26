058004020dc3d70634300b9bf5151f20d6bbac96
sopa <vnkvd571@yandex.ru>
2024-12-26 14:20:06 +0300

    Создать класс-модель Film.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,16 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
 public class Film {
+    String name;
+    String description;
+    LocalDate releaseDate;
+    Long duration;
 }
