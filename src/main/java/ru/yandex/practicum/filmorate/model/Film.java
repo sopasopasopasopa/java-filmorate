09bdc36156cf350d0bb31e561ccf92617ac90031
sopa <vnkvd571@yandex.ru>
2024-12-26 16:49:15 +0300

    Добавить поле id

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,9 +8,11 @@ import java.time.LocalDate;
 /**
  * Film.
  */
+@Builder
 @Data
 public class Film {
-    String name;
+    Long id;
+    String title;
     String description;
     LocalDate releaseDate;
     Long duration;
