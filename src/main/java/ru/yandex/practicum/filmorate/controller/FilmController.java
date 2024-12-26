66da60237a1fb0b2bb964278928e17a04bbdc015
sopa <vnkvd571@yandex.ru>
2024-12-26 16:51:30 +0300

    Реализовать эндпоинт POST(добавление фильма) и его логику.

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,11 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/films")
@@ -11,5 +15,10 @@ public class FilmController {
 
     public FilmController(FilmService filmService) {
         this.filmService = filmService;
+    }
+
+    @PostMapping
+    public Film filmCreate(@Valid @RequestBody Film film) {
+        return filmService.filmCreate(film);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
@@ -15,5 +16,47 @@ public class FilmService {
     private static final LocalDate MIN_RELESE_DATE = LocalDate.of(1895, 12, 28);
     private Map<Long, Film> films = new HashMap<>();
     private static Logger log = LoggerFactory.getLogger(FilmService.class);
+
+    public Film filmCreate(Film film) {
+        if (film.getTitle() == null || film.getTitle().isEmpty()) {
+            log.warn("Film title {}", film.getTitle());
+            throw new ValidationException("Film title should be empty");
+        }
+        if (film.getDescription().length() >= 200) {
+            log.warn("Film description {}", film.getDescription());
+            throw new ValidationException("Maximum description length — 200 characters");
+        }
+        if (film.getReleaseDate().isBefore(MIN_RELESE_DATE)) {
+            log.warn("Film releaseDate {}", film.getReleaseDate());
+            throw new ValidationException("Film release date: no earlier than 28.12.1895");
+        }
+        if (film.getDuration() == null || film.getDuration() <= 0) {
+            log.warn("Film duration {}", film.getDuration());
+            throw new ValidationException("Film duration should ot be negative");
+        }
+
+        Film createFilm = Film.builder()
+                .id(nextId())
+                .title(film.getTitle())
+                .description(film.getDescription())
+                .releaseDate(film.getReleaseDate())
+                .duration(film.getDuration())
+                .build();
+
+        films.put(createFilm.getId(), createFilm);
+
+        return createFilm;
+
+    }
+
+
+
+    private long nextId() {
+        return films.keySet()
+                .stream()
+                .mapToLong(Long::longValue)
+                .max()
+                .orElse(0L) + 1;
+    }
 
 }
