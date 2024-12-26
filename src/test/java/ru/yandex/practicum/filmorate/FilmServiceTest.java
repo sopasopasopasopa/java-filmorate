a5a5feebe0e92d72e37311f7b1e70de32e1a93ee
sopa <vnkvd571@yandex.ru>
2024-12-26 19:59:07 +0300

    Добавить тесты для FilmServiceTest

---
+++ src/test/java/ru/yandex/practicum/filmorate/FilmServiceTest.java
@@ -0,0 +1,66 @@
+package ru.yandex.practicum.filmorate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FilmServiceTest {
+
+    private FilmService filmService;
+
+    @BeforeEach
+    void setUp() {
+        filmService = new FilmService();
+    }
+
+    @Test
+    void shouldThrowExceptionWhenTitleIsEmpty() {
+        Film film = Film.builder()
+                .title("")
+                .build();
+        assertThrows(ValidationException.class, () -> filmService.filmCreate(film));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenDescriptionIsTooLong() {
+        String longDescription = "a".repeat(201);
+        Film film = Film.builder()
+                .title("Valid Title")
+                .description(longDescription)
+                .build();
+        assertThrows(ValidationException.class, () -> filmService.filmCreate(film));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenReleaseDateIsBeforeMinDate() {
+        Film film = Film.builder()
+                .title("Valid Title")
+                .releaseDate(LocalDate.of(1895, 12, 27))
+                .build();
+        assertThrows(ValidationException.class, () -> filmService.filmCreate(film));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenDurationIsNegative() {
+        Film film = Film.builder()
+                .title("Valid Title")
+                .duration(-1L)
+                .build();
+        assertThrows(ValidationException.class, () -> filmService.filmCreate(film));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenDurationIsZero() {
+        Film film = Film.builder()
+                .title("Valid Title")
+                .duration(0L)
+                .build();
+        assertThrows(ValidationException.class, () -> filmService.filmCreate(film));
+    }
+}
