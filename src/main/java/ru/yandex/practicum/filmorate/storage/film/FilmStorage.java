f9e13da6db1ac0cb2bdad4b15fe936d76bcd280a
sopa <vnkvd571@yandex.ru>
2025-01-14 12:41:30 +0300

    Создать интерфейсы FilmStorage и UserStorage, в которых будут определены методы добавления, удаления и модификации объектов.

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.storage.film;
+
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.List;
+
+public interface FilmStorage {
+    Film addFilm(Film film);
+    Film updateFilm(Film film);
+    List<Film> getAllFilms();
+    void deleteFilm(Long id);
+}
