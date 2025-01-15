74b89ab05382bbb012d9396a713828e514b6eab8
sopa <vnkvd571@yandex.ru>
2025-01-15 13:39:07 +0300

    Изменить создание объекта UserService.

--- src/test/java/ru/yandex/practicum/filmorate/UserServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserServiceTest.java
@@ -6,6 +6,7 @@ import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 
 import java.time.LocalDate;
 
@@ -18,7 +19,7 @@ public class UserServiceTest {
 
     @BeforeEach
     void setUp() {
-        userService = new UserService();
+        userService = new UserService(new InMemoryUserStorage());
     }
 
     @Test
