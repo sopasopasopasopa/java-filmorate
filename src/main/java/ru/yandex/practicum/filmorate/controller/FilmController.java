2b59462ead00caa0a9429f1254aac078a9dbfdef
sopa <vnkvd571@yandex.ru>
2025-01-15 13:30:24 +0300

    Добавить новые эндпоинты

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -4,17 +4,21 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.service.LikeService;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/films")
 public class FilmController {
 
     private final FilmService filmService;
+    private final LikeService likeService;
 
-    public FilmController(FilmService filmService) {
+    public FilmController(FilmService filmService, LikeService likeService) {
         this.filmService = filmService;
+        this.likeService = likeService;
     }
 
     @PostMapping
@@ -27,8 +31,33 @@ public class FilmController {
         return filmService.filmUpdate(updateFilm);
     }
 
+    @PutMapping("/{filmId}/like/{userId}")
+    public Set<Long> addLikeFilm(@PathVariable Long filmId, @PathVariable Long userId) {
+        return likeService.addLike(filmId, userId);
+    }
+
     @GetMapping
     public List<Film> getAllFilms() {
         return filmService.getAllFilms();
+    }
+
+    @GetMapping("/{filmId}")
+    public Film findById(@PathVariable Long filmId) {
+        return filmService.findById(filmId);
+    }
+
+    @GetMapping("/popular")
+    public List<Film> topFilms(@RequestParam(defaultValue = "10") Integer count) {
+        return likeService.topTenPopularMovies(count);
+    }
+
+    @DeleteMapping("/{filmId}/like/{userId}")
+    public Set<Long> deleteLike(@PathVariable Long filmId, @PathVariable Long userId) {
+        return likeService.deleteLike(filmId, userId);
+    }
+
+    @DeleteMapping("/{filmId}")
+    public void deleteFilmById(@PathVariable Long filmId) {
+        filmService.deleteFilmById(filmId);
     }
 }
