ade5a70b8f8cff163e135376b21c67721e8fe93c
sopa <vnkvd571@yandex.ru>
2025-04-13 20:37:12 +0300

    Обновить старые сервисы

--- src/main/java/ru/yandex/practicum/filmorate/service/FriendService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FriendService.java
@@ -1,11 +1,9 @@
 package ru.yandex.practicum.filmorate.service;
 
-import jakarta.validation.constraints.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
@@ -14,102 +12,76 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class FriendService {
-
-    private static Logger log = LoggerFactory.getLogger(FriendService.class);
     private final UserStorage userStorage;
 
+    @Autowired
     public FriendService(UserStorage userStorage) {
         this.userStorage = userStorage;
     }
 
-    public Set<Long> addFriends(@NotNull Long userId, @NotNull Long friendId) {
-        log.info("Attempting to add friend: user ID: {}, friend ID: {}", userId, friendId);
+    public void addFriend(Long userId, Long friendId) {
+        User user = getUserById(userId);
+        User friend = getUserById(friendId);
 
-        User user = userStorage.getUserById(userId);
-        User friend = userStorage.getUserById(friendId);
-
-        if (user == null || friend == null) {
-            log.error("User with ID {} or friend with ID {} not found", userId, friendId);
-            throw new NotFoundException("User with Id: " + userId + " or friend with Id " + friendId + " not found");
-        }
-        if (user.getFriends().contains(friendId)) {
-            log.error("User with ID {} is already a friend", friendId);
-            throw new ValidationException("User is already a friend");
+        // Проверяем существование дружбы в обе стороны
+        if (user.getFriends().contains(friendId) || friend.getFriends().contains(userId)) {
+            log.warn("Friendship between {} and {} already exists", userId, friendId);
+            return;
         }
 
+        // Добавляем взаимную дружбу
         user.getFriends().add(friendId);
         friend.getFriends().add(userId);
+
+        // Сохраняем обоих пользователей
         userStorage.updateUser(user);
         userStorage.updateUser(friend);
 
-        log.info("User {} successfully added user {} as a friend", userId, friendId);
-        log.info("Friend list of user {}: {}", user.getName(), user.getFriends());
-        log.info("Friend list of user {}: {}", friend.getName(), friend.getFriends());
-
-        return user.getFriends();
+        log.info("Mutual friendship created between {} and {}", userId, friendId);
     }
 
-    public Set<Long> deleteFriend(@NotNull Long userId, @NotNull Long friendId) {
-        User user = userStorage.getUserById(userId);
-        User friend = userStorage.getUserById(friendId);
+    public Set<Long> removeFriend(Long userId, Long friendId) {
+        User user = getUserById(userId);
+        User friend = getUserById(friendId);
 
-        if (user == null || friend == null) {
-            log.error("User with ID {} or friend with ID {} not found", userId, friendId);
-            throw new NotFoundException("User or friend with Id " + userId + " " + friendId + " not found");
+        // Удаляем дружбу в обе стороны
+        boolean removedFromUser = user.getFriends().remove(friendId);
+        boolean removedFromFriend = friend.getFriends().remove(userId);
+
+        if (!removedFromUser && !removedFromFriend) {
+            log.warn("No friendship found between {} and {}", userId, friendId);
+            return user.getFriends();
         }
 
-        user.getFriends().remove(friendId);
-        friend.getFriends().remove(userId);
-
+        // Сохраняем изменения
         userStorage.updateUser(user);
         userStorage.updateUser(friend);
 
-        log.info("User {} successfully removed user {} from their friends", userId, friendId);
-
+        log.info("Mutual friendship removed between {} and {}", userId, friendId);
         return user.getFriends();
     }
 
-
-    public List<User> getAllFriends(@NotNull Long userId) {
-        User user = userStorage.getUserById(userId);
-        if (user == null) {
-            log.error("User with ID {} not found", userId);
-            throw new NotFoundException("User with Id " + userId + " not found.");
-        }
-        log.info("User {} requested the list of friends: {}", userId, user.getFriends());
-
-        Set<Long> friendsIds = user.getFriends();
-        List<User> friendList = friendsIds.stream()
-                .map(userStorage::getUserById)
-                .toList();
-        return friendList;
-    }
-
-    public List<User> listMutualFriends(@NotNull Long userId, @NotNull Long friendId) {
-        log.info("Attempting to find mutual friends between users with ID: {} and {}", userId, friendId);
-        User user = userStorage.getUserById(userId);
-        User friend = userStorage.getUserById(friendId);
-
-        if (user == null || friend == null) {
-            log.error("User with ID {}, or friend with ID {} not found", userId, friendId);
-            throw new NotFoundException("One of the users with ID: " + userId + " or " + friendId + " not found");
-        }
-
-        Set<Long> mutualFriendIds = findMutualFriends(user, friend);
-        List<User> mutualFriends = mutualFriendIds.stream()
-                .map(userStorage::getUserById)
+    public List<User> getFriends(Long userId) {
+        User user = getUserById(userId);
+        return user.getFriends().stream()
+                .map(this::getUserById)
                 .collect(Collectors.toList());
-
-        return mutualFriends;
     }
 
-    private Set<Long> findMutualFriends(User user, User friend) {
-        Set<Long> friendsOfUser = user.getFriends();
-        friendsOfUser.retainAll(friend.getFriends());
-        return friendsOfUser;
+    public List<User> getCommonFriends(Long userId, Long otherUserId) {
+        User user = getUserById(userId);
+        User otherUser = getUserById(otherUserId);
+
+        return user.getFriends().stream()
+                .filter(friendId -> otherUser.getFriends().contains(friendId))
+                .map(this::getUserById)
+                .collect(Collectors.toList());
     }
 
-
-
+    private User getUserById(Long userId) {
+        return userStorage.getUserById(userId)
+                .orElseThrow(() -> new NotFoundException("User with ID " + userId + " not found"));
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/LikeService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/LikeService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
@@ -16,8 +17,7 @@ import java.util.Set;
 
 @Service
 public class LikeService {
-
-    private static Logger log = LoggerFactory.getLogger(LikeService.class);
+    private static final Logger log = LoggerFactory.getLogger(LikeService.class);
     private final FilmStorage filmStorage;
     private final UserStorage userStorage;
 
@@ -29,53 +29,59 @@ public class LikeService {
     public Set<Long> addLike(@NotNull Long filmId, @NotNull Long userId) {
         log.debug("Adding like from user {} to film {}", userId, filmId);
 
-        Film likedFilm = filmStorage.getFilmById(filmId);
-        if (likedFilm == null) {
-            log.error("Film with ID {} not found", filmId);
-            throw new ValidationException("Film not found");
-        }
-        if (userStorage.getUserById(userId) == null) {
-            log.error("User  with ID {} who is adding the like does not exist", userId);
-            throw new NotFoundException("User with Id " + userId + " who wants to like does not exist");
-        }
+        Film likedFilm = filmStorage.getFilmById(filmId)
+                .orElseThrow(() -> {
+                    log.error("Film with ID {} not found", filmId);
+                    return new ValidationException("Film not found");
+                });
+
+        User user = userStorage.getUserById(userId)
+                .orElseThrow(() -> {
+                    log.error("User with ID {} who is adding the like does not exist", userId);
+                    return new NotFoundException("User with Id " + userId + " not found");
+                });
+
         if (!likedFilm.getUserLikes().add(userId)) {
-            log.warn("User  {} has already liked film {}", userId, filmId);
+            log.warn("User {} has already liked film {}", userId, filmId);
             throw new ValidationException("User can only like a film once");
         }
 
         likedFilm.setLikes(likedFilm.getLikes() + 1);
         filmStorage.updateFilm(likedFilm);
 
-        log.info("User  {} successfully liked film {}", userId, filmId);
-
+        log.info("User {} successfully liked film {}", userId, filmId);
         return likedFilm.getUserLikes();
     }
 
     public Set<Long> deleteLike(@NotNull Long filmId, @NotNull Long userId) {
         log.debug("Removing like from user {} to film {}", userId, filmId);
 
-        Film removeLikeForFilm = filmStorage.getFilmById(filmId);
-        if (removeLikeForFilm == null) {
-            log.warn("Film with ID {} does not exist", filmId);
-            throw new NotFoundException("Film with Id " + filmId + " does not exist");
-        }
-        if (userStorage.getUserById(userId) == null || !removeLikeForFilm.getUserLikes().remove(userId)) {
-            log.error("User  with ID {} who is removing the like does not exist", userId);
-            throw new NotFoundException("User with Id " + userId + " who wants to remove like does not exist");
+        Film film = filmStorage.getFilmById(filmId)
+                .orElseThrow(() -> {
+                    log.warn("Film with ID {} does not exist", filmId);
+                    return new NotFoundException("Film with Id " + filmId + " not found");
+                });
+
+        User user = userStorage.getUserById(userId)
+                .orElseThrow(() -> {
+                    log.error("User with ID {} who is removing the like does not exist", userId);
+                    return new NotFoundException("User with Id " + userId + " not found");
+                });
+
+        if (!film.getUserLikes().remove(userId)) {
+            throw new NotFoundException("Like not found");
         }
 
-        removeLikeForFilm.setLikes(removeLikeForFilm.getLikes() - 1);
-        filmStorage.updateFilm(removeLikeForFilm);
-        log.debug("User  {} successfully removed like from film {}", userId, filmId);
+        film.setLikes(film.getLikes() - 1);
+        filmStorage.updateFilm(film);
+        log.debug("User {} successfully removed like from film {}", userId, filmId);
 
-        return removeLikeForFilm.getUserLikes();
+        return film.getUserLikes();
     }
 
-    public List<Film> topTenPopularMovies(Integer count) {
-        log.debug("Number of films to create the list {}", count);
-
+    public List<Film> getTopFilms(Integer count) {
+        log.debug("Getting top {} films", count);
         List<Film> films = filmStorage.getAllFilms();
-        log.debug("List of films for sorting {}", films);
 
         return films.stream()
                 .sorted(Comparator.comparingInt(Film::getLikes).reversed())
