0189fe157eb2ef29dba7c06d7dcb74e394546eed
sopa <vnkvd571@yandex.ru>
2025-04-13 20:36:45 +0300

    Создать новые интерфейс

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mpa/MpaDao.java
@@ -0,0 +1,10 @@
+package ru.yandex.practicum.filmorate.storage.mpa;
+
+import ru.yandex.practicum.filmorate.model.Mpa;
+import java.util.List;
+import java.util.Optional;
+
+public interface MpaDao {
+    List<Mpa> getAllMpa();
+    Optional<Mpa> getMpaById(int id);
+}
