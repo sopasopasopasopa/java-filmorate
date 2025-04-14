c5a794ec4f76d63836572d77b113ff51e1da9175
sopa <vnkvd571@yandex.ru>
2025-04-14 23:05:35 +0300

    Исправить аннотацию

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,6 +1,10 @@
 package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Data;
 
@@ -12,10 +16,19 @@ import java.util.Set;
 @Data
 public class User {
     Long id;
-    String email;
-    String login;
+
+    @Email(message = "Некорректный формат email")
+    private String email;
+
+
+    @NotBlank(message = "Логин не может быть пустым")
+    @Pattern(regexp = "\\S+", message = "Логин не может содержать пробелы")
+    private String login;
+
     String name;
-    LocalDate birthday;
+
+    @PastOrPresent(message = "Дата рождения не может быть в будущем") // Добавить
+    private LocalDate birthday;
 
     @JsonIgnore
     @Builder.Default
