b299edaa2d44c9c1a4a9445f40f457e48d87332f
sopa <vnkvd571@yandex.ru>
2025-04-14 23:07:15 +0300

    Обновление данных

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,19 +1,17 @@
--- MPA Ratings
+MERGE INTO genres (genre_id, name) VALUES
+(1, 'Комедия'),
+(2, 'Драма'),
+(3, 'Мультфильм'),
+(4, 'Триллер'),
+(5, 'Документальный'),
+(6, 'Боевик');
+
 MERGE INTO mpa (mpa_id, name) VALUES
 (1, 'G'),
 (2, 'PG'),
 (3, 'PG-13'),
 (4, 'R'),
 (5, 'NC-17');
-
--- Genres
-MERGE INTO genres (genre_id, name) VALUES
-(1, 'Comedy'),
-(2, 'Drama'),
-(3, 'Animation'),
-(4, 'Thriller'),
-(5, 'Documentary'),
-(6, 'Action');
 
 -- Sample Users (добавлен user 3)
 INSERT INTO users (email, login, name, birthday) VALUES
